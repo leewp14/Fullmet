fd5fd4bb83a85205f6168f46e4f5221cd546ea88
lee.wp14 <lee.wp14@gmail.com>
2021-07-28 18:33:35 +0800

    Fullmet: version 0.01

---
+++ Fullmet.user.js
@@ -0,0 +1,79 @@
+// ==UserScript==
+// @name         Fullmet
+// @namespace    honami
+// @version      0.01
+// @description  Enables HTML5 video controls on Google Meet's in-meeting video presentation. 
+// @author       lee.wp14
+// @source       https://github.com/leewp14/Fullmet
+// @updateURL    https://github.com/leewp14/Fullmet/raw/main/Fullmet.user.js
+// @supportURL   https://github.com/leewp14/Fullmet/issues
+// @match        https://meet.google.com/*
+// @grant        none
+// @runat        document-body
+// @noframes
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // global variables
+    var honami_FullmetNode = null;
+    var honami_FullmetInlineStyle = '';
+    honami_FullmetInlineStyle += "video{pointer-events: initial !important;}";  // video
+    honami_FullmetInlineStyle += ".uAzxg{bottom: 1rem; padding: 1rem 0; height: unset; top: unset;}";  // participant tooltip
+    var honami_FullmetClassDelay = 2000;
+
+    // internal variables
+    var honamiInternals_debug_enabled = true;
+    var honamiInternals_debug_label = '[DEBUG] leewp14/Fullmet';
+
+    honamiInternals_debug('script started');
+    honami_init();
+    honami_injectStyle();
+    setInterval(honami_videoNode_injectClass, honami_FullmetClassDelay);
+    honamiInternals_debug('script ended');
+
+    // functions
+    function honami_init(){
+        // create style element
+        honamiInternals_debug('exec honami_init()');
+
+        honami_FullmetNode = document.createElement('style');
+        honami_FullmetNode.id = 'honami_FullmetNode';
+        honami_FullmetNode.innerHTML = honami_FullmetInlineStyle;
+        
+        honamiInternals_debug('exit honami_init()');
+    }
+
+    function honami_injectStyle(){
+        // inject style element
+        honamiInternals_debug('exec honami_injectStyle()');
+        
+        if(honami_FullmetNode && document.body){
+            document.body.appendChild(honami_FullmetNode);
+        }
+
+        honamiInternals_debug('exit honami_injectStyle()');
+    }
+
+    function honami_videoNode_injectClass(){
+        // set 'controls' attribude to video elements
+        honamiInternals_debug('exec honami_videoNode_injectClass()');
+        var videoNode = document.getElementsByTagName('video');
+        if(videoNode){
+            for(var i = 0; i < videoNode.length; i++){
+                videoNode[i].setAttribute('controls', '');
+            };
+        }
+        honamiInternals_debug('exit honami_videoNode_injectClass()');
+    }
+
+    // internal functions
+    function honamiInternals_debug(type){
+        // log debugging messages
+        if(honamiInternals_debug_enabled){
+            console.log(honamiInternals_debug_label + ': ' + type ?? 'undefined action');
+        }
+    }
+
+})();
---
+++ README.md
@@ -0,0 +1,14 @@
+## Fullmet
+A simple Tampermonkey userscript that enables HTML5 video controls on Google Meet's in-meeting video presentation. 
+
+-----
+
+### Features
+- TLDR: allows you to toggle fullscreen when a presentation is ongoing
+- Moves the distracting participant tooltip (the 3-in-1 unpin, mute, remove button)
+
+### Install
+Grab the latest copy from here: [Install for Tampermonkey](https://github.com/leewp14/Fullmet/raw/main/Fullmet.user.js)
+
+### Issues
+Face any issues or would like to help in improvising the script? Create an issue and I will get back to you soon!
